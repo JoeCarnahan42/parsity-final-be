2d30bf13761e32b5f213dd5badfc97a40ca25420
Joe Carnahan <carnahanjoseph@yahoo.com>
2025-04-26 14:32:06 -0500

    add create user route for db population

--- routes/main.js
+++ routes/main.js
@@ -1,9 +1,25 @@
 const router = require("express").Router();
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-// TODO - make a .env and link it here
-// TODO - add db logic
+
+// Connect DB
 const pool = require("../dataBase/db");
+
+router.post("/create-user", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const newUser = await pool.query(
+      "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *",
+      [email, password]
+    );
+    res.json(newUser.rows[0]);
+  } catch (err) {
+    console.error;
+    err.message;
+    res.status(500).json({ error: "Server Error" });
+  }
+});
 
 const JWT_KEY = process.env.SECRET_KEY;
 
