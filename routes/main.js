cb389767bd1397e8f55abb0476de337cb62ebebd
Joe Carnahan <carnahanjoseph@yahoo.com>
2025-05-01 13:21:32 -0500

    refactor POST projects route to a batch-input for adding all project data

--- routes/main.js
+++ routes/main.js
@@ -63,19 +63,145 @@ router.get("/projects/:id", authenticate, async (req, res) => {
   }
 });
 
+// TODO - re structure initial project POST
 router.post("/projects", authenticate, async (req, res) => {
-  const { title, customer, state } = req.body;
-  try {
-    const newProject = await pool.query(
-      "INSERT INTO projects (title, customer, state) VALUES ($1, $2, $3) RETURNING *",
-      [title, customer, state]
-    );
-    res.status(200).json(newProject.rows[0]);
-  } catch (err) {
-    res.status(400).json({ message: err });
+  const client = await pool.connect();
+
+  const {
+    title,
+    customer,
+    state,
+    projectManagers,
+    tasks,
+    projMetrics,
+    purchaseList,
+  } = req.body;
+
+  if (
+    !title ||
+    !customer ||
+    !state ||
+    !Array.isArray(projectManagers) ||
+    !projectManagers.length ||
+    !Array.isArray(tasks) ||
+    !tasks.length ||
+    !Array.isArray(projMetrics) ||
+    !projMetrics.length ||
+    !Array.isArray(purchaseList) ||
+    !purchaseList.length
+  ) {
+    return res.status(400).json({ message: "All fields are required." });
   }
 
-  res.status(201).json({ message: "Project created", data: newProject });
+  try {
+    await client.query("BEGIN");
+    const addToProjects = await client.query(
+      "INSERT INTO projects (title, customer, state) VALUES ($1, $2, $3) RETURNING id",
+      [title, customer, state]
+    );
+    const projectId = addToProjects.rows[0].id;
+
+    if (Array.isArray(tasks) && tasks.length > 0) {
+      const values = [];
+      const params = [];
+
+      tasks.forEach((task, i) => {
+        const idx = i * 7;
+        values.push(
+          `($${idx + 1}, $${idx + 2}, $${idx + 3}, $${idx + 4}, $${idx + 5}, $${
+            idx + 6
+          }, $${idx + 7})`
+        );
+        params.push(
+          projectId,
+          task.title,
+          task.partNumber,
+          task.material,
+          task.hours,
+          task.status,
+          task.created
+        );
+      });
+      await client.query(
+        `INSERT INTO in_house_tasks (project_id, title, partnumber, material, hours, status, created_at) VALUES ${values.join(
+          ", "
+        )}`,
+        params
+      );
+    }
+
+    if (Array.isArray(projectManagers) && projectManagers.length > 0) {
+      const values = [];
+      const params = [];
+
+      projectManagers.forEach((pm, i) => {
+        const idx = i * 3;
+        values.push(`($${idx + 1}, $${idx + 2}, $${idx + 3})`);
+        params.push(projectId, pm.name, pm.title);
+      });
+      await client.query(
+        `INSERT INTO project_managers (project_id, name, title) VALUES ${values.join(
+          ", "
+        )}`,
+        params
+      );
+    }
+
+    if (Array.isArray(projMetrics) && projMetrics.length > 0) {
+      const values = [];
+      const params = [];
+
+      projMetrics.forEach((metric, i) => {
+        const idx = i * 4;
+        values.push(`($${idx + 1}, $${idx + 2}, $${idx + 3}, $${idx + 4})`);
+        params.push(projectId, metric.money, metric.hours, metric.due);
+      });
+      await client.query(
+        `INSERT INTO projected_metrics (project_id, budget_money, budget_hours, due_date) VALUES ${values.join(
+          ", "
+        )}`,
+        params
+      );
+    }
+
+    if (Array.isArray(purchaseList) && purchaseList.length > 0) {
+      const values = [];
+      const params = [];
+      // Specific logic here
+      purchaseList.forEach((item, i) => {
+        const idx = i * 6;
+        values.push(
+          `($${idx + 1}, $${idx + 2}, $${idx + 3}, $${idx + 4}, $${idx + 5}, $${
+            idx + 6
+          })`
+        );
+        params.push(
+          projectId,
+          item.title,
+          item.partNumber,
+          item.description,
+          item.orderedOn,
+          item.price
+        );
+      });
+      await client.query(
+        `INSERT INTO purchase_list (project_id, title, partnumber, description, ordered_on, price) VALUES ${values.join(
+          ", "
+        )}`,
+        params
+      );
+    }
+    await client.query("COMMIT");
+    res
+      .status(200)
+      .json({ message: "Project Created Successfully!", projectId });
+  } catch (err) {
+    await client.query("ROLLBACK");
+    console.error("Transaction Failed", err);
+    res.status(500).json({ message: "Project Creation Failed" });
+  } finally {
+    client.release();
+  }
 });
 
 // adds a new in house task to a defined project
