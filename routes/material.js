dab390986c2c458d403607e8334d4284a9692b73
Joe Carnahan <carnahanjoseph@yahoo.com>
2025-05-13 11:37:02 -0500

    Add TODO to finish material route

---
+++ routes/material.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+require("dotenv").config();
+const pool = require("../dataBase/db");
+
+const authenticate = require("../middleware/authenticate");
+
+// TODO - finish route
