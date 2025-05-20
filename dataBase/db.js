87489f6cd9379d1351c76fa8849650f9e9196079
Joe Carnahan <carnahanjoseph@yahoo.com>
2025-05-20 15:12:20 -0500

    Update db schema to work with internal render connection

--- dataBase/db.js
+++ dataBase/db.js
@@ -5,7 +5,7 @@ const PASSWORD = process.env.PASSWORD;
 
 const pool = new Pool({
   user: "wolf42",
-  host: "dpg-d0mcbdemcj7s7396gl90-a.ohio-postgres.render.com",
+  host: "dpg-d0mcbdemcj7s7396gl90-a",
   database: "projectsdb_yi76",
   password: PASSWORD,
   port: 5432,
