352af35727391b45ac5e3e4edad8ada6229ad89e
Joe Carnahan <carnahanjoseph@yahoo.com>
2025-05-21 14:15:35 -0500

    Rollback CORS changes

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
-  res.header("Access-Control-Allow-Credentials: true");
+  res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
 
