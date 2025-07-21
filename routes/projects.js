5332a092b65fcd3e6894788bcb02faba0f17a691
JoeCarnahan42 <carnahanjoseph@yahoo.com>
2025-07-21 09:59:18 -0500

    change error response for projects PUT routes.

--- routes/projects.js
+++ routes/projects.js
@@ -347,7 +347,7 @@ router.put("/:id", authenticate, async (req, res) => {
     );
     res.status(200).json(updatedProjState.rows[0]);
   } catch (err) {
-    console.error(err);
+    console.error(err.message);
     res.status(500).json({ message: "Database Error" });
   }
 });
