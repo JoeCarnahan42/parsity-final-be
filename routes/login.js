8f2d65ac2fb683cd22300aa8f4ac40f91c011d3c
JoeCarnahan42 <carnahanjoseph@yahoo.com>
2025-07-29 16:20:38 -0500

    fix response

--- routes/login.js
+++ routes/login.js
@@ -33,8 +33,8 @@ router.get(
       maxAge: 3600000,
     });
 
-    res.redirect("http://localhost:3000");
     res.json({ message: "Logged in", user: { email, firstName: first_name } });
+    res.redirect("http://localhost:3000");
   }
 );
 
