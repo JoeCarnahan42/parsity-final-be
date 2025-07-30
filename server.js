197a1819e990ffc8f126b95b2ba6d90055e84e74
JoeCarnahan42 <carnahanjoseph@yahoo.com>
2025-07-30 01:07:17 -0500

    trying a different origin

--- server.js
+++ server.js
@@ -33,15 +33,9 @@ const materialRoutes = require("./routes/material");
 
 const allowedOrigin = "https://parsity-final-fe.vercel.app";
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", allowedOrigin);
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
+cors({
+  origin: "https://parsity-final-fe.vercel.app",
+  credentials: true,
 });
 
 app.use(
