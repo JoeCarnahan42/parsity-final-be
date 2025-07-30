390deb6bfb300f6d5e5b0877c68dba440d404ddf
JoeCarnahan42 <carnahanjoseph@yahoo.com>
2025-07-30 11:46:35 -0500

    change callback redirect

--- routes/login.js
+++ routes/login.js
@@ -22,16 +22,7 @@ router.get(
     console.log("Cookies:", req.cookies); // if using cookie-parser
     console.log("User:", req.user); // should also be available
     req.session.save(() => {
-      res.send(`
-        <html>
-          <body>
-            <script>
-              window.opener.postMessage({ loggedIn: true }, "https://parsity-final-fe.vercel.app");
-              window.close();
-            </script>
-          </body>
-        </html>
-      `);
+      res.redirect("https://parsity-final-fe.vercel.app/oauth-success");
     });
   }
 );
