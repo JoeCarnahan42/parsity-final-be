45492b047139f123554907ed94034dc55f397dea
JoeCarnahan42 <carnahanjoseph@yahoo.com>
2025-07-30 13:24:45 -0500

    change allowed origin to new fe host

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.set("trust proxy", 1);
 
 app.use(
   cors({
-    origin: "https://parsity-final-fe.vercel.app",
+    origin: "https://parsity-final-fe.onrender.com/",
     credentials: true,
   })
 );
